4df18d373cb4926f83627ab6a6d4516f195cda16
Ty Jenkins <cientifik85@gmail.com>
2020-08-11 19:05:23 -0400

    targets document elements, initial function code for media stream

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('testing');
+const videoElement = document.querySelector('#video');
+const button = document.querySelector('#button');
+
+// Async function prompting user to select media stream, pass to video element and play video
+aynsc function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    } catch(error) {
+
+    }
+}
