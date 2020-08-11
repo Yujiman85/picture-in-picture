5e09e81ef2db8f708e663a253f9604176fe49e12
Ty Jenkins <cientifik85@gmail.com>
2020-08-11 19:24:49 -0400

    media is now able to be shared to video element on page

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const videoElement = document.querySelector('#video');
 const button = document.querySelector('#button');
 
 // Async function prompting user to select media stream, pass to video element and play video
-aynsc function selectMediaStream() {
+async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        // Returns true when video is fully loaded
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch(error) {
 
     }
 }
+
+// On Load
+selectMediaStream();
